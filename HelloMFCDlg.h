dbdfe83545473fb15da8d4c732d49d9d311b9c8e
mrliux <mrliuxchn@gmail.com>
2020-10-12 04:34:24 +0800

    commit all file

---
+++ HelloMFCDlg.h
@@ -0,0 +1,38 @@
+﻿
+// HelloMFCDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CHelloMFCDlg 对话框
+class CHelloMFCDlg : public CDialogEx
+{
+// 构造
+public:
+	CHelloMFCDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HELLOMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton3();
+//	afx_msg void OnBnDoubleclickedButton2();
+	afx_msg void OnBnClickedButton2();
+};
